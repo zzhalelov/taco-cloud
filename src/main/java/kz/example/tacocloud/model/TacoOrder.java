47f5dc0677c687ed9018ebf1695eb4079e91da00
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-04-25 22:44:05 +0500

    feat: dependencies jdbc & h2

--- src/main/java/kz/example/tacocloud/model/TacoOrder.java
+++ src/main/java/kz/example/tacocloud/model/TacoOrder.java
@@ -6,11 +6,16 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
-public class TacoOrder {
+public class TacoOrder implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private Date placedAt;
 
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
